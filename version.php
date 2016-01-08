736e876a193557c161655b7aa9023dbfe9358531
schlender <kontakt@marcoschlender.de>
2016-01-08 14:05:44 +0100

    update for moodle 3.0, change from module to plugin, add component attribute

--- version.php
+++ version.php
@@ -17,7 +17,7 @@
 
 
 /**
- * Defines the version of newmodule
+ * Defines the version of newplugin
  *
  * This code fragment is called by moodle_needs_upgrading() and
  * /admin/index.php
@@ -30,7 +30,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version  = 2011082900;    // If version == 0 then module will not be installed
-//$module->version  = 2010032200;  // The current module version (Date: YYYYMMDDXX)
-$module->requires = 2010031900;    // Requires this Moodle version
-$module->cron     = 15;            // Period for cron to check this module (secs)
+$plugin->version  = 2011082900;    // If version == 0 then plugin will not be installed
+//$plugin->version  = 2010032200;  // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires = 2010031900;    // Requires this Moodle version
+$plugin->cron     = 15;            // Period for cron to check this plugin (secs)
+$plugin->component = "mod_digitalization";
